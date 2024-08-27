2c0a298feff3a850f77b75d820c1c40da5e30d13
Gustavo Nunes Viana <nunesvianagustavo@gmail.com>
2024-08-27 17:22:18 -0300

    update dumpfile.sql

--- bd/dump.sql
+++ bd/dump.sql
@@ -16,171 +16,125 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `atividades_nao_programadas`
+-- Table structure for table `unscheduled_activities`
 --
 
-DROP TABLE IF EXISTS `atividades_nao_programadas`;
+DROP TABLE IF EXISTS `unscheduled_activities`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `atividades_nao_programadas` (
-  `id_atividade_nao_prog` int NOT NULL AUTO_INCREMENT,
-  `nome_atividade` varchar(50) NOT NULL,
-  `justificativa` varchar(500) NOT NULL,
-  `horas_totais` int NOT NULL,
-  `classificacao` set('ensino','pesquisa','extensao') NOT NULL,
-  `horas_ensino` int NOT NULL DEFAULT '0',
-  `horas_pesquisa` int NOT NULL DEFAULT '0',
-  `horas_extensao` int NOT NULL DEFAULT '0',
-  `ano` year NOT NULL,
-  `id_relatorio` int NOT NULL,
-  `email_grupo_pet` varchar(50) NOT NULL,  -- Changed from int to varchar(50)
-  PRIMARY KEY (`id_atividade_nao_prog`),
-  KEY `id_relatorio` (`id_relatorio`),
-  KEY `email_grupo_pet` (`email_grupo_pet`),
-  CONSTRAINT `atividades_nao_programadas_ibfk_1` FOREIGN KEY (`id_relatorio`) REFERENCES `relatorios` (`id_relatorio`),
-  CONSTRAINT `atividades_nao_programadas_ibfk_2` FOREIGN KEY (`email_grupo_pet`) REFERENCES `grupos_pet` (`email`)
+CREATE TABLE `unscheduled_activities` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `justification` varchar(1000) NOT NULL,
+  `total_hours` int NOT NULL,
+  `teaching_hours` int NOT NULL DEFAULT '0',
+  `research_hours` int NOT NULL DEFAULT '0',
+  `extension_hours` int NOT NULL DEFAULT '0',
+  `report_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `report_id` (`report_id`),
+  CONSTRAINT `unscheduled_activities_ibfk_1` FOREIGN KEY (`report_id`) REFERENCES `reports` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `atividades_nao_programadas`
+-- Table structure for table `scheduled_activities`
 --
 
-LOCK TABLES `atividades_nao_programadas` WRITE;
-/*!40000 ALTER TABLE `atividades_nao_programadas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `atividades_nao_programadas` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `atividades_programadas`
---
-
-DROP TABLE IF EXISTS `atividades_programadas`;
+DROP TABLE IF EXISTS `scheduled_activities`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `atividades_programadas` (
-  `id_atividade_prog` int NOT NULL AUTO_INCREMENT,
-  `nome_atividade` varchar(50) NOT NULL,
-  `realizacao` enum('nao','parcial','total') NOT NULL,
-  `horas_totais` int NOT NULL,
-  `classificacao` set('ensino','pesquisa','extensao') DEFAULT NULL,
-  `horas_ensino` int NOT NULL DEFAULT '0',
-  `horas_pesquisa` int NOT NULL DEFAULT '0',
-  `horas_extensao` int NOT NULL DEFAULT '0',
-  `ano` year NOT NULL,
-  `id_relatorio` int NOT NULL,
-  `email_grupo_pet` varchar(50) NOT NULL,  -- Changed from int to varchar(50)
-  PRIMARY KEY (`id_atividade_prog`),
-  KEY `id_relatorio` (`id_relatorio`),
-  KEY `email_grupo_pet` (`email_grupo_pet`),
-  CONSTRAINT `atividades_programadas_ibfk_1` FOREIGN KEY (`id_relatorio`) REFERENCES `relatorios` (`id_relatorio`),
-  CONSTRAINT `atividades_programadas_ibfk_2` FOREIGN KEY (`email_grupo_pet`) REFERENCES `grupos_pet` (`email`)
+CREATE TABLE `scheduled_activities` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `carrying_out` enum('not','partial','full') NOT NULL,
+  `total_hours` int NOT NULL,
+  `teaching_hours` int NOT NULL DEFAULT '0',
+  `research_hours` int NOT NULL DEFAULT '0',
+  `extension_hours` int NOT NULL DEFAULT '0',
+  `report_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `report_id` (`report_id`),
+  CONSTRAINT `scheduled_activities_ibfk_1` FOREIGN KEY (`report_id`) REFERENCES `reports` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `atividades_programadas`
+-- Table structure for table `groups`
 --
 
-LOCK TABLES `atividades_programadas` WRITE;
-/*!40000 ALTER TABLE `atividades_programadas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `atividades_programadas` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `grupos_pet`
---
-
-DROP TABLE IF EXISTS `grupos_pet`;
+DROP TABLE IF EXISTS `groups`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `grupos_pet` (
-  `nome` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
+CREATE TABLE `groups` (
+  `email` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `instagram` varchar(100) DEFAULT NULL,
-  `pagina` varchar(100) DEFAULT NULL,
-  `nof_bolsistas` int NOT NULL,
-  `nof_voluntarios` int NOT NULL,
-  `endereco` varchar(100) DEFAULT NULL,
-  `campus` enum('Araranguá','Florianópolis','Curitibanos','Joinvile','Blumenau') NOT NULL,
-  `centro` enum('CTS','CTE','CCR','CTC','CCB','CTJ','CCA','CCE','CCS','CCJ','CDS','CED','CFH','CFM','CSE','PROGRAD') NOT NULL,
-  `email_tutor` varchar(50) DEFAULT NULL,/*todo remove this default, it should be not null*/
+  `webpage` varchar(100) DEFAULT NULL,
+  `nof_scholarships` int NOT NULL,
+  `nof_volunteers` int NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `campus` enum('Araranguá','Florianópolis','Curitibanos','Joinville','Blumenau') NOT NULL,
+  `center` enum('CTS','CTE','CCR','CTC','CCB','CTJ','CCA','CCE','CCS','CCJ','CDS','CED','CHF','CFM','CSE') NOT NULL,
+  `email_tutor` varchar(100) NOT NULL,
   PRIMARY KEY (`email`),
   KEY `email_tutor` (`email_tutor`),
-  CONSTRAINT `grupos_pet_ibfk_1` FOREIGN KEY (`email_tutor`) REFERENCES `usuarios` (`email`)
+  CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`email_tutor`) REFERENCES `tutors` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `grupos_pet`
+-- Table structure for table `reports`
 --
 
-LOCK TABLES `grupos_pet` WRITE;
-/*!40000 ALTER TABLE `grupos_pet` DISABLE KEYS */;
-/*!40000 ALTER TABLE `grupos_pet` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `relatorios`
---
-
-DROP TABLE IF EXISTS `relatorios`;
+DROP TABLE IF EXISTS `reports`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `relatorios` (
-  `id_relatorio` int NOT NULL AUTO_INCREMENT,
-  `cond_infra` enum('melhorou','manteve','piorou') NOT NULL,
-  `desc_infra` varchar(500) NOT NULL,
-  `cond_equip` enum('melhorou','manteve','piorou') NOT NULL,
-  `desc_equip` varchar(500) NOT NULL,
-  `cond_custeio` enum('sim','nao') NOT NULL,
-  `desc_custeio` varchar(500) NOT NULL,
-  `publico_alvo_dentro` varchar(1000) NOT NULL,
-  `publico_alvo_fora` varchar(1000) NOT NULL,
-  `cont_permanencia` varchar(1000) NOT NULL,
-  `cont_inclusao` varchar(1000) NOT NULL,
-  `desc_articulacao_politica` varchar(1000) NOT NULL,
-  `desc_articulacao_atividades` varchar(1000) NOT NULL,
-  `ano` year NOT NULL,
-  `id_grupo` varchar(50) NOT NULL,  -- Changed from int to varchar(50)
-  PRIMARY KEY (`id_relatorio`),
-  KEY `id_grupo` (`id_grupo`),
-  CONSTRAINT `relatorios_ibfk_1` FOREIGN KEY (`id_grupo`) REFERENCES `grupos_pet` (`email`)
+CREATE TABLE `reports` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `activities_articulation` varchar(1000) NOT NULL,
+  `politics_articulation` varchar(1000) NOT NULL,
+  `selection_students` varchar(1000) NOT NULL,
+  `permanence_students` varchar(1000) NOT NULL,
+  `ufsc_target_public` varchar(1000) NOT NULL,
+  `society_target_public` varchar(1000) NOT NULL,
+  `infrastructure_condition` enum('better','equal','worse') NOT NULL,
+  `infrastructure_description` varchar(500) NOT NULL,
+  `tools_condition` enum('better','equal','worse') NOT NULL,
+  `tools_description` varchar(500) NOT NULL,
+  `costing_condition` enum('yes','no') NOT NULL,
+  `costing_description` varchar(500) NOT NULL,
+  `year` year NOT NULL,
+  `group_email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `group_email` (`group_email`),
+  CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`group_email`) REFERENCES `groups` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `relatorios`
+-- Table structure for table `tutors`
 --
 
-LOCK TABLES `relatorios` WRITE;
-/*!40000 ALTER TABLE `relatorios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `relatorios` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuarios`
---
-
-DROP TABLE IF EXISTS `usuarios`;
+DROP TABLE IF EXISTS `tutors`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuarios` (
-  `email` varchar(50) NOT NULL,
+CREATE TABLE `tutors` (
+  `email` varchar(100) NOT NULL,
   `senha` varchar(60) NOT NULL,
   `nome` varchar(100) NOT NULL,
-  `membro_claa` enum('nao','suplente','titular') NOT NULL,
+  `status_claa` enum('no','substitute','holder') NOT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usuarios`
+-- Dumping data for tables
 --
 
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+LOCK TABLES `unscheduled_activities`, `scheduled_activities`, `groups`, `reports`, `tutors` WRITE;
+/*!40000 ALTER TABLE `unscheduled_activities`, `scheduled_activities`, `groups`, `reports`, `tutors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `unscheduled_activities`, `scheduled_activities`, `groups`, `reports`, `tutors` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
